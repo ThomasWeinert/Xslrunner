d98ef4f2c9dcc8394772c9c3be2b9adb500dc509
Thomas Weinert <thomas@weinert.info>
2011-07-27 21:17:49 +0200

    Tested: Callback\LoadDocument

---
+++ tests/Callback/LoadDocumentTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Carica\Xsl\Runner\Callback;
+
+use \Carica\Xsl\Runner as Runner;
+
+include_once(__DIR__.'/../TestCase.php');
+
+class LoadDocumentTest extends Runner\TestCase {
+
+  /**
+  * @covers \Carica\Xsl\Runner\Callback\LoadDocument::execute
+  */
+  public function testExecute() {
+    $callback = new LoadDocument();
+    $doc = $callback->execute(array(__DIR__.'/TestData/simple.xml'));
+    $this->assertEquals(
+      '<success/>',
+      $doc->saveXml($doc->documentElement)
+    );
+  }
+}
