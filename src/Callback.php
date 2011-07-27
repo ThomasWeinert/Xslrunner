86a7fe5cfa1f865a563b27987f885e872b59098e
Thomas Weinert <thomas@weinert.info>
2011-07-27 21:26:05 +0200

    Documented: Callback interface and Callback\LoadDocument class

--- src/Callback.php
+++ src/Callback.php
@@ -1,7 +1,30 @@
 <?php
+/**
+* Interface definition for xsl callback classes. The callbacks can be executed from xslt
+* using a generic registered php function.
+*
+* @license http://www.opensource.org/licenses/mit-license.php The MIT License
+* @copyright Copyright (c) 2011 Thomas Weinert
+*
+* @package XslRunner
+*/
 
 namespace Carica\Xsl\Runner;
 
+/**
+* Interface definition for xsl callback classes. The callbacks can be executed from xslt
+* using a generic registered php function.
+*
+* @package XslRunner
+*/
 interface Callback {
-  function execute($arguments);
+
+  /**
+  * Execute is called from the generic php callback function. The $arguments array contains all
+  * parameters of the call.
+  *
+  * @param array $arguments
+  * @return scalar|DOMNode
+  */
+  function execute(array $arguments);
 }
--- src/Callback/LoadDocument.php
+++ src/Callback/LoadDocument.php
@@ -1,12 +1,31 @@
 <?php
+/**
+* Xslt Callback object. Create a new DOMDocument, load the given xml file and return the document.
+*
+* @license http://www.opensource.org/licenses/mit-license.php The MIT License
+* @copyright Copyright (c) 2011 Thomas Weinert
+*
+* @package XslRunner
+*/
 
 namespace Carica\Xsl\Runner\Callback;
 
 use \Carica\Xsl\Runner as Runner;
 
+/**
+* Xslt Callback object. Create a new DOMDocument, load the given xml file and return the document.
+*
+* @package XslRunner
+*/
 class LoadDocument implements Runner\Callback  {
 
-  public function execute($arguments) {
+  /**
+  * Create a new DOMDocument, load the given xml file and return the document.
+  *
+  * @param array $arguments
+  * @return DOMDocument
+  */
+  public function execute(array $arguments) {
     $dom = new \DOMDocument();
     $dom->load($arguments[0]);
     return $dom;
